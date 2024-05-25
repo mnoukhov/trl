91b5bc588c0dbd0cb8625616bba07c5499304642
Michael Noukhovitch <mnoukhov@gmail.com>
2024-05-25 00:26:00 +0000

    avg logprob for ipo

--- trl/trainer/dpo_trainer.py
+++ trl/trainer/dpo_trainer.py
@@ -527,7 +527,7 @@ class DPOTrainer(Trainer):
         all_logps = self._get_batch_logps(
             all_logits,
             concatenated_batch["concatenated_labels"],
-            average_log_prob=False,
+            average_log_prob=(self.loss_type == "ipo"),
         )
 
         chosen_logps = all_logps[:len_chosen]
